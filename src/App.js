8001b389bb6316ddbbe52bdb04ef73cc3c35d6a4
codingChewie <codingChewie@gmail.com>
2020-12-17 17:15:20 -0600

    extracting a config module

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import http from './services/httpService'
+import config from './config.json'
 import './App.css'
-
-const apiEndpoint = 'https://jsonplaceholder.typicode.com/posts'
 
 class App extends Component {
   state = {
@@ -10,13 +9,13 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    const { data: posts } = await http.get(apiEndpoint)
+    const { data: posts } = await http.get(config.apiEndpoint)
     this.setState({ posts })
   }
 
   handleAdd = async () => {
     const obj = { title: 'a', body: 'b' }
-    const { data: post } = await http.post(apiEndpoint, obj)
+    const { data: post } = await http.post(config.apiEndpoint, obj)
 
     const posts = [post, ...this.state.posts]
     this.setState({ posts })
@@ -24,7 +23,7 @@ class App extends Component {
 
   handleUpdate = async post => {
     post.title = 'UPDATED'
-    http.put(`${apiEndpoint}/${post.id}`, post)
+    http.put(`${config.apiEndpoint}/${post.id}`, post)
     const posts = [...this.state.posts]
     const index = posts.indexOf(post)
     posts[index] = { ...post }
@@ -38,7 +37,7 @@ class App extends Component {
     this.setState({ posts })
 
     try {
-      await http.delete(apiEndpoint + '/' + post.id)
+      await http.delete(config.apiEndpoint + '/' + post.id)
     } catch (err) {
       if (err.response && err.response.status === 404) {
         alert('This post has already been deleted')
